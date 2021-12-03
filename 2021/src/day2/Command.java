2ffbf14c4d8b6f37a0ea48bebffc547dbea019d9
jelmer <geojelmer@gmail.com>
2021-12-03 22:21:19 +0100

    dir structure

---
+++ 2021/src/day2/Command.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class Command {
+
+    public String direction;
+    public Integer amount;
+
+    public Command(String direction, Integer amount) {
+        this.direction = direction;
+        this.amount = amount;
+    }
+}
---
+++ 2021/src/parser/AdventParser.java
@@ -0,0 +1,66 @@
+package parser;
+
+import day2.Command;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class AdventParser {
+
+    private File f;
+    private Scanner s;
+
+    public AdventParser(String fileName) {
+        try {
+
+            f = new File(fileName);
+            s = new Scanner(f);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Integer> day1() {
+
+        List<Integer> l = new ArrayList<>();
+
+        while (s.hasNextLine()) {
+            l.add(Integer.parseInt(s.nextLine()));
+        }
+
+        return l;
+    }
+
+    public List<Command> day2() {
+
+        List<Command> l = new ArrayList<>();
+
+        while (s.hasNextLine()) {
+
+            String[] tmp = s.nextLine().split("\\s+");
+
+            l.add(new Command(
+                    tmp[0],
+                    Integer.parseInt(tmp[1])
+            ));
+        }
+
+        return l;
+    }
+
+    public List<String> day3() {
+
+        List<String> l = new ArrayList<>();
+
+        while(s.hasNextLine()) {
+            l.add(s.nextLine());
+        }
+
+        return l;
+    }
+
+}
